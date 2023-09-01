2ced577a6c5b462073023205a1f7481724ebc7d9
ragnarok2022 <j7samsungek@gmail.com>
2023-09-01 15:16:18 -0300

    Coisas do Pc Antigo

---
+++ html/scripts.js
@@ -0,0 +1,25 @@
+    // Variável e seleção de elementos
+        
+    const apiKey = "d97a63981b979a167d9bd834c6e791aa";
+    const apiCountryuURL = "../icones/BR.png";
+
+    const cityInput = document.querySelector("#city-input");
+    const searchBtn = document.querySelector("#search");
+
+    // Funções 
+
+
+
+    // Eventos 
+    searchBtn.addEventListener("ci=lick" , (e) => {e.preventDefault();
+
+    const city = cityInput.valeu;
+
+    console.log(city)
+
+
+    })
+
+    function pesquisa(){
+        console.log("botão Fucionanto" )
+    }
